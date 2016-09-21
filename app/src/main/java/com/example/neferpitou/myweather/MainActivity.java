f9fdac6c446bbc7cf68ed3448ff88344c5da3d2b
lindong <6512314@qq.com>
2016-09-21 09:51:18 +0800

    first commit

---
+++ app/src/main/java/com/example/neferpitou/myweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.neferpitou.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Neferpitou on 2016/9/20.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
